c66269337cd40e8be4be9100f848610858a8e2a1
well148 <1486050855@qq.com>
2021-11-26 22:07:49 +0800

    项目初始化-引入vant

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+// 注册使用vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
